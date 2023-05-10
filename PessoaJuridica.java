48b9bf173a4299c244338791272ecea315923c7a
Gustavo Costa <126887704+GustavoMiec@users.noreply.github.com>
2023-05-10 12:15:16 -0300

    Create PessoaJuridica.java

---
+++ PessoaJuridica.java
@@ -0,0 +1,58 @@
+package treina;
+
+public class PessoaJuridica extends Pessoa{
+	
+	private String razaoSocial;
+	private String cnpj;
+	
+	
+	
+	
+	
+	public PessoaJuridica(String nome, String email, int idade, double renda, String razaoSocial, String cnpj) {
+		super(nome, email, idade, renda);
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+	}
+
+
+
+
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+
+
+
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+
+
+
+
+	public PessoaJuridica() {
+		super();
+	}
+
+
+
+
+
+	public PessoaJuridica(String razaoSocial, String cnpj) {
+		super();
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+	}
+	
+	public String identificador(){
+		return"PJ";
+	}
+
+	
+	
+}
