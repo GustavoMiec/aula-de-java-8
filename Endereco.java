244f9d95a58fde1aabcb9c8d2c8b5fb62ebc3258
Gustavo Costa <126887704+GustavoMiec@users.noreply.github.com>
2023-05-10 12:14:32 -0300

    Create Endereco.java

---
+++ Endereco.java
@@ -0,0 +1,22 @@
+package treina;
+
+public class Endereco {
+
+	private String logradouro;
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public Endereco(String logradouro) {
+		super();
+		this.logradouro = logradouro;
+	}
+
+	public Endereco() {
+		super();
+	}
+	
+	
+	
+}
